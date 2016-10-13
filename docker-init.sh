772c1eacc75c324c5c158f3c366c7590ae8f424a
jitakirin <jitakirin@gmail.com>
2016-10-13 13:10:58 +0100

    Do not quote VERBOSE variable expansion
    If not set it evaluates to '""' i.e. an empty parameter, which if used
    with `set` causes it to unset all positional parameters (`$@`).

--- docker-init.sh
+++ docker-init.sh
@@ -15,12 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e "${VERBOSE:+-x}"
+set -e ${VERBOSE:+-x}
 
 # We assume source of the program to benchmark is in CWD
 # Prepare the source
 if [[ -n $SETUP ]]; then
-  bash "${VERBOSE:+-x}" -c "${SETUP}"
+  bash ${VERBOSE:+-x} -c "${SETUP}"
 fi
 
 # All args are passed to temci
